29c01de45af78aa52f8284737e512194c8f992bc
Pietro Gagliardi <pietro10@mac.com>
2016-04-03 16:54:40 -0400

    Identified potential attachments.

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
+	"bytes"
 	"strings"
+	"net/mail"
+	"mime"
+	"mime/multipart"
 
 	"github.com/mxk/go-imap/imap"
+"encoding/hex"
+"github.com/davecgh/go-spew/spew"
 )
+
+var TODO_remove_this = spew.Config
+var TODO_remove_this_too = hex.Dump
 
 func start(cmd *imap.Command, err error) *imap.Command {
 	if err != nil {
@@ -34,6 +44,55 @@ func handle(cmd *imap.Command, err error) *imap.Command {
 	return finish(cmd)
 }
 
+func extract(raw *imap.Response) (msg *mail.Message, body []byte) {
+	info := raw.MessageInfo()
+	headerbytes := imap.AsBytes(info.Attrs["RFC822.HEADER"])
+	if len(headerbytes) == 0 {
+		panic("no header in message")
+	}
+	msg, err := mail.ReadMessage(bytes.NewReader(headerbytes))
+	if err != nil {
+		panic(err)
+	}
+	body = imap.AsBytes(info.Attrs["BODY[]"])
+	return msg, body
+}
+
+func canHaveAttachment(msg *mail.Message) (can bool, boundary string) {
+	mimetype, parts, err := mime.ParseMediaType(msg.Header.Get("Content-Type"))
+	if err != nil {
+		panic(err)
+	}
+	// this is valid; thanks to kurushiyama in irc.freenode.net/#go-nuts
+	can = strings.HasPrefix(mimetype, "multipart/")
+	if can {
+		boundary = parts["boundary"]
+	}
+	return
+}
+
+func process(raw *imap.Response) {
+	msg, body := extract(raw)
+	can, boundary := canHaveAttachment(msg)
+	if !can {
+		return
+	}
+	fmt.Println(msg.Header.Get("Subject"))
+	r := multipart.NewReader(bytes.NewReader(body), boundary)
+	for {
+		part, err := r.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("- %s\n  %s\n",
+			part.FileName(),
+			part.FormName())
+	}
+}
+
 func search(c *imap.Client, path string, indent int) {
 	handle(c.Select(path, true))
 	defer func() {		// closure needed as otherwise c.Close() will be run immediately
@@ -52,17 +111,16 @@ func search(c *imap.Client, path string, indent int) {
 	if err != nil {
 		panic(err)
 	}
-	list := start(c.Fetch(seq, "RFC822.HEADER"))
+	list := start(c.Fetch(seq, "RFC822.HEADER BODY[]"))
 	for list.InProgress() {
 		err = c.Recv(-1)
 		if err != nil {
 			panic(err)
 		}
-/*		for _, msg := range list.Data {
-			fmt.Printf("%s%s\n", strings.Repeat(" ", indent),
-				msg.Type)
+		for _, msg := range list.Data {
+			process(msg)
 		}
-*/		list.Data = nil
+		list.Data = nil
 		// TODO why is this needed?
 		c.Data = nil
 	}
